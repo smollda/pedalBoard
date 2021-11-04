f6eda1571e8cab25cc556d6bde4001a47c119abd
Václav Smola <smollda@seznam.cz>
2021-11-04 21:30:01 +0100

    Added joystick library, calibration process

--- src/main.cpp
+++ src/main.cpp
@@ -7,37 +7,35 @@
 #define DOUT1 11
 #define DOUT2 10
 #define DOUT3 9
+#define JOYSTICK_DEFAULT_REPORT_ID 0x06
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
 JOYSTICK_TYPE_JOYSTICK, 11, 0,
 true, true, true, true, true, true,
 true, true, true, true, true);
-
+long pedal1min = 0;
+long pedal1max = 0;
+long pedal2min = 0;
+long pedal2max = 0;
+long pedal3min = 0;
+long pedal3max = 0;
+long ruckaMin = 0;
+long ruckaMax = 0;
+long pedal1 = 0;
+long pedal2 = 0;
+long pedal3 = 0;
+long kalibraceZacatek;
+bool kalibrace = false;
+bool kalibraceStart = false;
+char recievedChar;
 bool is_ready = false; 
+long cekaniZacatek = 0;
+long cekani = 0;
   
-void setup() {
-  Joystick.begin();
-  pinMode(CLK1, OUTPUT); 
-  pinMode(CLK2, OUTPUT); 
-  pinMode(CLK3, OUTPUT); 
-  pinMode(DOUT1, INPUT);
-  pinMode(DOUT2, INPUT);
-  pinMode(DOUT3, INPUT); 
-  digitalWrite(CLK1, LOW);
-  digitalWrite(CLK2, LOW);
-  digitalWrite(CLK3, LOW);
-  Serial.begin(115200);
-  Joystick.setXAxisRange(-32767, 32767);
-  Joystick.setYAxisRange(-32767, 32767);
-  Joystick.setZAxisRange(-32767, 32767);
-  Joystick.setBrakeRange(-32767, 32767);
-}
-
 long EEPROMReadlong(long address) {
   long four = EEPROM.read(address);
   long three = EEPROM.read(address + 1);
   long two = EEPROM.read(address + 2);
   long one = EEPROM.read(address + 3);
- 
   return ((four << 0) & 0xFF) + ((three << 8) & 0xFFFF) + ((two << 16) & 0xFFFFFF) + ((one << 24) & 0xFFFFFFFF);
 }
 
@@ -70,7 +68,6 @@ long HX711read(int DOUT,int CLK)
 {
   // this waiting takes most time...
   while (digitalRead(DOUT) == HIGH) yield();
-  
   union
   {
     long value = 0;
@@ -102,21 +99,125 @@ long HX711read(int DOUT,int CLK)
 
   // SIGN extend
   if (v.data[2] & 0x80) v.data[3] = 0xFF;
-
-  //_lastRead = millis();
   return v.value;
+}
+
+void setup() {
+  Joystick.begin();
+  pinMode(CLK1, OUTPUT); 
+  pinMode(CLK2, OUTPUT); 
+  pinMode(CLK3, OUTPUT); 
+  pinMode(DOUT1, INPUT);
+  pinMode(DOUT2, INPUT);
+  pinMode(DOUT3, INPUT); 
+  digitalWrite(CLK1, LOW);
+  digitalWrite(CLK2, LOW);
+  digitalWrite(CLK3, LOW);
+  Serial.begin(115200);
+  Serial.println("Setup");
+  if (EEPROMReadlong(0) == -1)
+  {  
+    kalibrace = true;
+    kalibraceStart = true;
+    kalibraceZacatek = 0;
+  }
+  else
+  {
+    pedal1min = EEPROMReadlong(0);
+    pedal1max = EEPROMReadlong(4);
+    pedal2min = EEPROMReadlong(8);
+    pedal2max = EEPROMReadlong(12);
+    pedal3min = EEPROMReadlong(16);
+    pedal3max = EEPROMReadlong(20);
+    ruckaMin = EEPROMReadlong(24);
+    ruckaMax = EEPROMReadlong(28);
+  }
+  Joystick.setXAxisRange(-32767, 32767);
+  Joystick.setYAxisRange(-32767, 32767);
+  Joystick.setZAxisRange(-32767, 32767);
+  Joystick.setBrakeRange(-32767, 32767);
 }
 
 
 void loop() {
-  //Serial.print(HX711read(DOUT3,CLK3));
-  //Serial.print("                 ");
-  Serial.print(HX711read(DOUT2,CLK2));
-  Serial.print("                 ");
-  Serial.println(HX711read(DOUT1,CLK1));
-  //Joystick.setXAxis(constrain(prepocet(440000,720000,HX711read(DOUT3,CLK3)),-32767,32767));  // Brzda
-  delay(5);
-  
+  pedal1 = HX711read(DOUT1,CLK1);
+  pedal2 = HX711read(DOUT2,CLK2);
+  //pedal3 = HX711read(DOUT3,CLK1);
+  //Serial.println(cekani);
+  Joystick.setXAxis(constrain(prepocet(pedal1min,pedal1max,pedal1),-32767,32767));
+  Joystick.setYAxis(constrain(prepocet(pedal2min,pedal2max,pedal2),-32767,32767));
+  Joystick.setZAxis(constrain(prepocet(pedal3min,pedal3max,pedal3),-32767,32767));
+  //Načtení znaku z konzole pro spuštění kalibrace
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    recievedChar = Serial.read();
+
+    // say what you got:
+    if (recievedChar == 'k'){
+      kalibrace = true;
+      kalibraceStart = true;
+      kalibraceZacatek = millis();
+      Serial.println("Kalibrace:");
+      recievedChar = " ";
+    }
+  }
+  //Start kalibrace
+  if (kalibraceStart)
+  {
+    pedal1min = pedal1;
+    pedal2min = pedal2;
+    pedal3min = pedal3;
+    //ruckaMin = results[3];
+    pedal1max = 0;
+    pedal2max = 0;
+    pedal3max = 0;
+    ruckaMax = 0;
+    Serial.println("Start kalibrace");
+    Serial.print("Minimální hodnoty: Pedál 1:");
+    Serial.print(pedal1min);
+    Serial.print(" Pedál 2:");
+    Serial.print(pedal2min);
+    Serial.print(" Pedál 3:");
+    Serial.print(pedal3min);
+    Serial.print(" Ručka:");
+    Serial.println(ruckaMin);
+    Serial.println("Sešlápni opakovaně všechny 3 pedály + ručku na doraz do 10 sekund (brzdu dle libosti)");
+    kalibraceStart = false;
+  }
+  if(kalibrace){
+    if(pedal1 > pedal1max){
+      pedal1max = pedal1;
+    }
+    if(pedal2 > pedal2max){
+      pedal2max = pedal2;
+    }
+    if(pedal3 > pedal3max){
+      pedal3max = pedal3;
+    }
+    //  if(results[3] > ruckaMax){
+    //    ruckaMax = results[3];
+    //  }
+  }
+  if (millis() > (kalibraceZacatek + 10000) && kalibrace == true){
+    kalibrace = false;
+    EEPROMWritelong(0,pedal1min);
+    EEPROMWritelong(4,pedal1max);
+    EEPROMWritelong(8,pedal2min);
+    EEPROMWritelong(12,pedal2max);
+    EEPROMWritelong(16,pedal3min);
+    EEPROMWritelong(20,pedal3max);
+    EEPROMWritelong(24,ruckaMin);
+    EEPROMWritelong(28,ruckaMax);
+    Serial.println("Konec kalibrace!");
+    Serial.print("Maximální hodnoty: Pedál 1:");
+    Serial.print(pedal1max);
+    Serial.print(" Pedál 2:");
+    Serial.print(pedal2max);
+    Serial.print(" Pedál 3:");
+    Serial.print(pedal3max);
+    Serial.print(" Ručka:");
+    Serial.println(ruckaMax);
+  }
 
 }
 
